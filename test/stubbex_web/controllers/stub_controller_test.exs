81514812c9dab8336a857176cf641e838c2d35f6
Yawar Amin <yawar.amin@gmail.com>
2018-11-07 23:13:58 -0500

    Add stubbex cookie test

--- test/stubbex_web/controllers/stub_controller_test.exs
+++ test/stubbex_web/controllers/stub_controller_test.exs
@@ -1,13 +1,24 @@
 defmodule StubbexWeb.StubControllerTest do
   use StubbexWeb.ConnCase
 
+  @bla "/stubs/http/bla"
+
   # Some of these tests require an internet connection. They can be
   # excluded by running
   # `stubbex_stubs_dir=test mix test --exclude network:true`
 
   test "a non-existent endpoint should return a 'not implemented' response", %{conn: conn} do
-    conn = get conn, "/stubs/http/bla"
+    conn = get conn, @bla
     assert response(conn, 501) === ""
+  end
+
+  test "stubs must have 'stubbex' cookie set", %{conn: conn} do
+    %{resp_headers: headers} = get conn, @bla
+
+    assert Enum.any?(headers, fn
+             {"set-cookie", "stubbex=" <> _cookie} -> true
+             _else -> false
+           end)
   end
 
   @tag :network
